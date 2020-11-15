43d03e95015fcc21f64fed99f5382fe836ad02b2
Ryan Bonial <ryanbonial@gmail.com>
2020-11-15 12:47:54 -0700

    Try a shorter redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const fetchOptions = { headers: { 'Accept': 'application/json' }};
   const [dadJoke, setDadJoke] = useState('');
   const getDadJoke = async () => {
-    const dadResp = await fetch('/api/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
+    const dadResp = await fetch('/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
     // const dadResp = await fetch('https://icanhazdadjoke.com/search?page=1&limit=20&term=apple', fetchOptions);
     const dadJokeResults = await dadResp.json();
     console.log(dadJokeResults.results)
