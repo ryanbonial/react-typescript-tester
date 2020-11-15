957046e996c6780ff48fa87edb8b8281f0bc93e5
Ryan Bonial <ryanbonial@gmail.com>
2020-11-15 13:32:08 -0700

    Try serach terms

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,17 @@ import './App.css';
 function App() {
   const fetchOptions = { headers: { 'Accept': 'application/json' }};
   const [dadJoke, setDadJoke] = useState('');
-  const getDadJoke = async () => {
-    const dadResp = await fetch('/api/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
+  const [searchTerm, setSearchTerm] = useState('');
+  const getRandomDadJoke = async () => {
+    const dadResp = await fetch('/api/dad-joke/', fetchOptions);
+    // const dadResp = await fetch('https://icanhazdadjoke.com', fetchOptions);
+    const dadJokeResults = await dadResp.json();
+    console.log(dadJokeResults)
+    setDadJoke(dadJokeResults.joke);
+  }
+
+  const searchDadJoke = async () => {
+    const dadResp = await fetch(`/api/dad-joke/search?term=${searchTerm}`, fetchOptions);
     // const dadResp = await fetch('https://icanhazdadjoke.com', fetchOptions);
     const dadJokeResults = await dadResp.json();
     console.log(dadJokeResults)
@@ -15,9 +24,17 @@ function App() {
 
   return (
     <div className="App">
-        <h3>Typescript and yarn</h3>
+        <h3>Typescript, yarn, and _redirects</h3>
         <p>{dadJoke}</p>
-        <button onClick={() => getDadJoke()}>Click for dad joke via proxy</button>
+        <button onClick={() => getRandomDadJoke()}>Click for a random dad joke via proxy</button>
+        <hr />
+        <label htmlFor="joke">
+          Joke Search Term
+          <input type="search" value={searchTerm} onChange={e => setSearchTerm(e.target.value)}/>
+        </label>
+        <div>
+        <button onClick={() => searchDadJoke()}>Click for a dad joke via proxy about {searchTerm}</button>
+        </div>
     </div>
   );
 }
