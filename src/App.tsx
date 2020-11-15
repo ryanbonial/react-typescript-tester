358f839e89a93b2094ac7f20bf502becb6df3ac4
Ryan Bonial <ryanbonial@gmail.com>
2020-11-15 12:56:08 -0700

    ooh, it's working...try adding a bit more

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   const fetchOptions = { headers: { 'Accept': 'application/json' }};
   const [dadJoke, setDadJoke] = useState('');
   const getDadJoke = async () => {
-    const dadResp = await fetch('/dad-joke', fetchOptions);
+    const dadResp = await fetch('/api/dad-joke', fetchOptions);
     // const dadResp = await fetch('https://icanhazdadjoke.com', fetchOptions);
     const dadJokeResults = await dadResp.json();
     console.log(dadJokeResults)
