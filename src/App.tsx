d4d5031f3a34a8a69bf92763cc02884a64325cf5
Ryan Bonial <ryanbonial@gmail.com>
2020-11-14 19:13:08 -0700

    Adds dad joke

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
 import './App.css';
 
 function App() {
+  const fetchOptions = { headers: { 'Accept': 'application/json' }};
+  const [dadJoke, setDadJoke] = useState('');
+  const getDadJoke = async () => {
+    // const dadResp = await fetch('/api/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
+    const dadResp = await fetch('https://icanhazdadjoke.com/search?page=1&limit=20&term=apple', fetchOptions);
+    const dadJokeResults = await dadResp.json();
+    console.log(dadJokeResults.results)
+    setDadJoke(dadJokeResults.results[0].joke);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Typescript and yarn
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h3>Typescript and yarn</h3>
+        <p>{dadJoke}</p>
+        <button onClick={() => getDadJoke()}>Click for dad joke via proxy</button>
     </div>
   );
 }
