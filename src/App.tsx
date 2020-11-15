12615c6f7c0249b8b15ed5d41f33dbee362c9c17
Ryan Bonial <ryanbonial@gmail.com>
2020-11-14 19:14:29 -0700

    fetch joke through proxy redirect

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   const fetchOptions = { headers: { 'Accept': 'application/json' }};
   const [dadJoke, setDadJoke] = useState('');
   const getDadJoke = async () => {
-    // const dadResp = await fetch('/api/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
-    const dadResp = await fetch('https://icanhazdadjoke.com/search?page=1&limit=20&term=apple', fetchOptions);
+    const dadResp = await fetch('/api/dad-joke/search?page=1&limit=20&term=cake', fetchOptions);
+    // const dadResp = await fetch('https://icanhazdadjoke.com/search?page=1&limit=20&term=apple', fetchOptions);
     const dadJokeResults = await dadResp.json();
     console.log(dadJokeResults.results)
     setDadJoke(dadJokeResults.results[0].joke);
